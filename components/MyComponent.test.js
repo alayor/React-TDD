6e91f306ddd83500912d01effab84cadb2caacff
alayor <alayor3@gmail.com>
2017-10-28 00:05:08 -0500

    Add missing enzyme dependencies

--- components/MyComponent.test.js
+++ components/MyComponent.test.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import MyComponent from './MyComponent';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("MyComponent", () => {
   it("should render correctly", () => {
