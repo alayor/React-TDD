6c9b265c93d6c19f038f7140438280f1c46c5be2
alayor <alayor3@gmail.com>
2017-10-28 00:13:34 -0500

    Move components to src folder

--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import MyComponent from './MyComponent';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe("MyComponent", () => {
   it("should render correctly", () => {
